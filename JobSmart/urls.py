3d2e1d4d0addeb5bf2143ee890f4cc91b4bd9a34
Sebastian Castaño <scastanoa1@eafit.edu.co>
2024-09-25 23:42:01 -0500

    subir hoja de vida y guardarla

--- JobSmart/urls.py
+++ JobSmart/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('vacantes/', vacantesViews.vacantes,name='vacantes'),
     path('',resumesViews.showHomepage, name='home'),
     path('resume/',resumesViews.uploadResume, name='resumes'),
+    path('resultado/', resumesViews.resultado, name='resultado'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- resumes/views.py
+++ resumes/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Resume
+import pdfplumber
+
 
 # Create your views here.
-def uploadResume(request):
-    return render(request,'resume.html')
 def showHomepage(request):
     return render(request,'home.html' )
+
+def leer_pdf(pdf_file):
+    """Función que utiliza pdfplumber para extraer el texto de un PDF"""
+    texto = ""
+    with pdfplumber.open(pdf_file) as pdf:
+        for page in pdf.pages:
+            texto += page.extract_text()
+    return texto
+
+def uploadResume(request):
+    if request.method == 'POST' and request.FILES.get('pdf'):
+        pdf_file = request.FILES['pdf']
+        # Leer el contenido del PDF usando pdfplumber
+        texto = leer_pdf(pdf_file)
+
+        # Crear una nueva instancia de Resume y guardarla en la base de datos
+        resume = Resume(nombre=pdf_file.name, contenido=texto)
+        resume.save()
+
+        # Redirigir a otra vista o renderizar una respuesta
+        return redirect('resultado')
+
+    return render(request,'resume.html')
+
+def resultado(request):
+    # Puedes pasar información a la plantilla si lo necesitas
+    
+    return render(request, 'resultado.html')
