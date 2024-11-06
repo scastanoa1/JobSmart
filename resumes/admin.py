26c31d0425ac5c2993ba19cc0f81e7ec97ff5a64
Magdc <117609790+Magdc@users.noreply.github.com>
2024-11-05 21:09:04 -0500

    corregir error

--- resumes/admin.py
+++ resumes/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from .models import Resume
-<<<<<<< HEAD
-admin.site.register(Resume)
-=======
 
->>>>>>> 83b8d068588c3c1173352f1b63f1feb90e5e91aa
 # Register your models here.
 admin.site.register(Resume)
